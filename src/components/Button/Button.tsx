7c2bcdc018686982a08d34a3bf2acedf79e89fac
Virian <gstok@poczta.fm>
2026-09-10 00:38:10 +0200

    feat: add disabled state to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ import styles from './Button.module.scss';
 interface ButtonProps {
   className?: string;
   children: React.ReactNode;
+  disabled?: boolean;
   onClick?: () => void;
   variant?: 'primary' | 'secondary' | 'danger';
 }
@@ -11,11 +12,13 @@ interface ButtonProps {
 export const Button = ({
   className,
   children,
+  disabled = false,
   onClick = () => null,
   variant = 'primary',
 }: ButtonProps) => (
   <button
     className={clsx(styles.button, styles[`button-${variant}`], className)}
+    disabled={disabled}
     onClick={onClick}
   >
     {children}
