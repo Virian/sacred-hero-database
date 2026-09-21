9fd4923e8d680de71d69b0a2fdb0e3638a40b818
Virian <gstok@poczta.fm>
2026-09-21 19:21:17 +0200

    refactor: use rust-based approach for database handling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,30 +1,22 @@
 mod app_settings;
 mod characters;
 mod commands;
+mod database;
 mod save_reader;
 
 use tauri::Manager;
-use tauri_plugin_sql::{Migration, MigrationKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = vec![Migration {
-        version: 1,
-        description: "create_initial_tables",
-        sql: include_str!("../migrations/0000_init.sql"),
-        kind: MigrationKind::Up,
-    }];
-
     tauri::Builder::default()
-        .plugin(
-            tauri_plugin_sql::Builder::new()
-                .add_migrations("sqlite:database.sqlite", migrations)
-                .build(),
-        )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .manage(app_settings::SettingsState::default())
         .setup(|app| {
+            let database = tauri::async_runtime::block_on(database::initialize(app.handle()))
+                .expect("failed to initialize database");
+            app.manage(database);
+
             let settings_state = app.state::<app_settings::SettingsState>();
 
             if let Err(error) =
